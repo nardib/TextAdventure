3bf0471844c9efc415a104b9b6293d5b119eed94
Nardi <lorenzo.nardin@studenti.unipd.it>
2024-05-13 10:06:46 +0200

    Added some methods to Player class to set and get name and gender of the player, with a specific enum for 3 default gender

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,55 @@
+/*
+ * enum for all the accepted gender types
+*/
+enum Gender{
+    MALE,
+    FEMALE,
+    NEUTRAL
+}
+
 public class Player {
-    
+    /*
+     * state variables
+     */
+    private String name;
+    private Gender gender;
+
+    /*
+     * method to set the name of the player
+     */
+    public void setName(String n)
+    {
+        name = n;
+    }
+
+    /*
+     * Method to get the name of the player
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /*
+     * Method to set the gender of the player
+     */
+    public void setGender(String g)
+    {
+        if(g.toLowerCase().compareTo("m") == 0 || g.toLowerCase().compareTo("male") == 0)
+            gender = Gender.MALE;
+        else if (g.toLowerCase().compareTo("f") == 0 || g.toLowerCase().compareTo("female") == 0)
+            gender = Gender.FEMALE;
+        else if (g.toLowerCase().compareTo("n") == 0 || g.toLowerCase().compareTo("neutral") == 0)
+            gender = Gender.NEUTRAL;
+        else
+            throw new IllegalArgumentException("Invalid Gender");
+    }
+
+    /*
+     * Method to get the gender of the player
+     */
+    public Gender getGender()
+    {
+        return gender;
+    }
 }
