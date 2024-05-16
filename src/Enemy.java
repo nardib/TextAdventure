1ab91194953a7be377bb82837085f35698f17c17
Alessio Zanco <zancoalessio2003@gmail.com>
2024-05-16 16:41:29 +0200

    classe enemy

--- src/Enemy.java
+++ src/Enemy.java
@@ -9,11 +9,16 @@ public class Enemy {
     private Room currentRoom; 
     private int damage = 1; //number of HP decreased to the player every 1.5 seconds
     private boolean presence; //is the enemy in the map?
-    
+    private boolean[] aveilable = new boolean[4];
 
+    aveilable[0] = currentRoom.crossableNorth;
+    aveilable[1] = currentRoom.crossableNorth;
+    aveilable[2] = currentRoom.crossableNorth;
+    aveilable[3] = currentRoom.crossableNorth;
     public void move() 
     {
         int i = 4%(Integer.parseInt(Math.random()*4));
-        if()
+        if(aveilable[i]==true)
+            
     }
 }
