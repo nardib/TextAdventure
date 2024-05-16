6d0f6f39fa10351b773f3ff867c4d1a756be1d33
Nardi <lorenzo.nardin@studenti.unipd.it>
2024-05-16 09:44:20 +0200

    added weight to Item

--- src/Key.java
+++ src/Key.java
@@ -1,13 +1,14 @@
 import javax.swing.ImageIcon;
 
 public class Key extends Item {
-    
     /*
      * constructor
      */
     public Key()
     {
         icon = new ImageIcon(getClass().getResource("Key.jpeg"));
+        weight = 1;
     }
+
     
 }
