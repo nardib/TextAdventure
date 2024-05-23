25aaef9241aca5905160aae417ad469437eca855
Nardi <lorenzo.nardin@studenti.unipd.it>
2024-05-23 19:35:52 +0200

    Implemented undo method to Game class with memento pattern

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Game {
     /*
      * State variables
@@ -6,6 +9,7 @@ public class Game {
     private Map map;
     private Enemy enemy;
     private boolean gameIsOn;
+    private List<GameMemento> mementos = new ArrayList<>();
 
     /*
      * Constructor for initializing the game variables
@@ -20,40 +24,40 @@ public class Game {
     /*
      * Method to manage the player moves
      */
-    private void playerTurn(String input) {
+    private String playerTurn (String input) {
         if (input.equalsIgnoreCase("north")){
             player.changeDirection(Direction.NORTH);
-            return;
+            return "You are now facing north";
         }
         else if (input.equalsIgnoreCase("south")){
             player.changeDirection(Direction.SOUTH);
-            return;
+            return "You are now facing south";
         }
         else if (input.equalsIgnoreCase("east")){
             player.changeDirection(Direction.EAST);
-            return;
+            return "You are now facing east";
         }
         else if (input.equalsIgnoreCase("west")){
             player.changeDirection(Direction.WEST);
-            return;
+            return "You are now facing west";
         }
         else if(input.substring(0, 5).equalsIgnoreCase("cross"))
         {
             if (input.substring(6).equalsIgnoreCase("north") && map.getRoom(player.getCurrentRoom()).getCrossableNorth()){
                 player.setCurrentRoom(player.getCurrentRoom() + Player.CROSS_NORTH);
-                return;
+                return "You moved to the north room";
             }
             else if (input.substring(6).equalsIgnoreCase("south") && map.getRoom(player.getCurrentRoom()).getCrossableSouth()){
                 player.setCurrentRoom(player.getCurrentRoom() + Player.CROSS_SOUTH);
-                return;
+                return "You moved to the south room";
             }
             else if (input.substring(6).equalsIgnoreCase("east") && map.getRoom(player.getCurrentRoom()).getCrossableEast()){
                 player.setCurrentRoom(player.getCurrentRoom() + Player.CROSS_EAST);
-                return;
+                return "You moved to the east room";
             }
             else if (input.substring(6).equalsIgnoreCase("west") && map.getRoom(player.getCurrentRoom()).getCrossableWest()){
                 player.setCurrentRoom(player.getCurrentRoom() + Player.CROSS_WEST);
-                return;
+                return  "You moved to the west room";
             }
             throw new IllegalArgumentException("You can't cross in that direction");
         }
@@ -70,8 +74,9 @@ public class Game {
         */
         else if(input.substring(0, 3).equalsIgnoreCase("use"))
         {
-            return;
+            return "You used the " + input.substring(4);
         }
+        throw new IllegalArgumentException("Invalid input");
      }
 
     /*
@@ -90,10 +95,23 @@ public class Game {
      * Method to generate the actions of a move
      */
     public void nextMove(String input) {
-            // player turn
-            playerTurn(input);
-            // enemy turn
-            enemyTurn();
+        if (!gameIsOn) {
+            throw new IllegalArgumentException("Game is over");
+        }
+        else if (input.equalsIgnoreCase("undo")) {
+            undo();
+            return;
+        }
+        else if (input.equalsIgnoreCase("quit") || input.equalsIgnoreCase("exit")) {
+            gameIsOn = false;
+            return;
+        }
+        // player turn
+        playerTurn(input);
+        // enemy turn
+        enemyTurn();
+        //i save the state of the game after each move
+        saveCurrentState();
     }
 
     /*
@@ -109,5 +127,59 @@ public class Game {
 
     public Player getPlayer() {
         return player;
+    }
+
+    /*
+     * class for memento pattern to implement undo function
+     */
+    private class GameMemento {
+        private final Player player;
+        private final Enemy enemy;
+        private final Map map;
+        private final boolean gameIsOn;
+
+        public GameMemento(Player player, Enemy enemy, Map map, boolean gameIsOn) {
+            this.player = player;
+            this.enemy = enemy;
+            this.map = map;
+            this.gameIsOn = gameIsOn;
+        }
+
+        public Player getPlayer() {
+            return player;
+        }
+
+        public Enemy getEnemy() {
+            return enemy;
+        }
+
+        public Map getMap() {
+            return map;
+        }
+
+        public boolean getGameIsOn() {
+            return gameIsOn;
+        }
+    }
+
+    /*
+     * Method to save the current state of the game
+     */
+    public void saveCurrentState() {
+        mementos.add(new GameMemento(player, enemy, map, gameIsOn));
+    }
+
+    /*
+     * Method to undo the last move
+     */
+    public void undo() {
+        if (mementos.size() > 0) {
+            GameMemento memento = mementos.get(mementos.size() - 1);
+            player = memento.getPlayer();
+            enemy = memento.getEnemy();
+            map = memento.getMap();
+            gameIsOn = memento.getGameIsOn();
+            mementos.remove(mementos.size() - 1);
+        }
     }
 }
