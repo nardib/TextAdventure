de99623296ef6a07c412d4e75444b88af590318a
Nardi <lordnardin2003@gmail.com>
2024-06-07 14:28:03 +0200

    now you can execute the game with the command "mvn exec:java"

--- src/main/java/org/zssn/escaperoom/Main.java
+++ src/main/java/org/zssn/escaperoom/Main.java
@@ -13,10 +13,8 @@ public class Main{
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                new GameFrame(); // Crea un oggetto GameFrame e mostra l'interfaccia grafica
+                new GameFrame();
             }
         });
-
-        // Altre istruzioni della tua applicazione, se presenti
     }
 }
