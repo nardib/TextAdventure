7f913d839345ec3fabbbc0bf2ac5dcbdfb33e6d1
Alessio Zanco <zancoalessio2003@gmail.com>
2024-06-23 17:44:38 +0200

    Aggiunte tre nuove classi S3Utility, LoadGameState e SaveGameState

---
+++ src/main/java/org/zssn/escaperoom/S3Utility.java
@@ -0,0 +1,15 @@
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+public class S3Utility {
+    private static final Region REGION = Region.US_EAST_1; // La regione del mio bucket 
+    private static final S3Client s3 = S3Client.builder()
+            .region(REGION)
+            .credentialsProvider(ProfileCredentialsProvider.create())
+            .build();
+
+    public static S3Client getS3Client() {
+        return s3;
+    }
+}
